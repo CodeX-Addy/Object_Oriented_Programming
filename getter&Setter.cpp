25ff40a3fa694304b23e976d47785c22101d22f6
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-05 22:35:28 +0530

    Update getter&Setter.cpp

--- getter&Setter.cpp
+++ getter&Setter.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
-
 class Aditya{
     public:
     string hobby;
     private:
     int age;
-    
     public:
-    int getAge(){
+    int getAge(){//getter
         return age;
     }
     
@@ -18,7 +16,7 @@ int main() {
     Aditya quality;
     quality.hobby = "DarkAcademia";
     cout << quality.hobby << endl;
-    cout << quality.getAge() << endl;
+    cout << quality.getAge() << endl; //It'll not throw any error even if age is under the private access modifier
 
     return 0;
 }
