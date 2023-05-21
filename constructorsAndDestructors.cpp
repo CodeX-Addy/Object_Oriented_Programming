4cc5c045c19c0caddcf5b0cc1d1ca3bec37a5f5d
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-21 13:54:50 +0530

    Create constructorsAndDestructors.cpp

---
+++ constructorsAndDestructors.cpp
@@ -0,0 +1,17 @@
+// Constructors
+#include <iostream>
+using namespace std;
+
+class Addy{
+    public:
+    int age;
+    // Constructors
+    Addy(){
+        cout << "My name is Aditya!";
+    }
+  };
+
+int main() {
+    Addy a;
+    return 0;
+}
