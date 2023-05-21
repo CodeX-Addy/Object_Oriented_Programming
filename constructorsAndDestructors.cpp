9df02faa321687fa995ab61fb005b89f7da7e85b
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-21 13:56:28 +0530

    Update constructorsAndDestructors.cpp

--- constructorsAndDestructors.cpp
+++ constructorsAndDestructors.cpp
@@ -15,3 +15,24 @@ int main() {
     Addy a;
     return 0;
 }
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+class Addy{
+    
+    public:
+    int age;
+    // Parameterised constructor
+    Addy(int age){
+        cout << "Hey" << endl;
+        this -> age = age; // this keyword is a pointer which stores the address of an object
+        cout << age;
+    }
+};
+
+int main() {
+    Addy a;
+    return 0;
+}
