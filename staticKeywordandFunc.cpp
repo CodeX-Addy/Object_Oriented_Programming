80f93cd3603091778fd2e4aeb9b1b85773528d94
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-28 19:59:59 +0530

    Update staticKeywordandFunc.cpp

--- staticKeywordandFunc.cpp
+++ staticKeywordandFunc.cpp
@@ -4,13 +4,18 @@ using namespace std;
 class object{
     public:
     static int sizeofObject;
+    
+    static int func(){//Static function declaration
+        return sizeofObject;
+    }
 };
 
-int object::sizeofObject = 10; // :: called as scope resolution operator
+int object::sizeofObject = 10; //:: called as scope resolution operator
 
 int main(){
     cout << object::sizeofObject << endl;
     object o;
     o.sizeofObject = 20;
     cout << o.sizeofObject << endl;
+    cout << object::func() << endl;//Static function called
 }
