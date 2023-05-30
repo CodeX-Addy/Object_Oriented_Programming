a69b3fd11df2fb0a0ee559910599aa1e2a25d358
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-30 19:17:43 +0530

    Update inheritanceTypes.cpp

--- inheritanceTypes.cpp
+++ inheritanceTypes.cpp
@@ -45,3 +45,34 @@ int main() {
     S.loc();
     return 0;
 }
+
+// Multiple inheritance
+#include<iostream>
+using namespace std;
+
+class integer{
+    public:
+    int value;
+    void intclass(){
+        cout << "An integer class" << endl;
+    }
+};
+
+class floatn{
+    public:
+    float val;
+    void floatclass(){
+        cout << "A floating class" << endl;
+    }
+};
+
+class Hybrid : public integer, public floatn{
+    
+};
+
+int main(){
+    Hybrid h1;
+    h1.intclass();
+    h1.floatclass();
+    
+}
