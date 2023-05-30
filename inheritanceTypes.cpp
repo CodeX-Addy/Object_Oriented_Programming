db2be5d923b83af48a50c5f313225798153f78f3
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-30 18:59:22 +0530

    Update inheritanceTypes.cpp

--- inheritanceTypes.cpp
+++ inheritanceTypes.cpp
@@ -18,3 +18,30 @@ int main() {
     c.loc();
     return 0;
 }
+
+// Multilevel inheritance
+#include <iostream>
+using namespace std;
+
+class School {
+    public:
+    string locat;
+    
+    void loc(){
+        cout << "Opp.to metro station" << endl;
+    }
+};
+
+class Class : public School{
+    
+};
+
+class Student: public Class{
+    
+};
+
+int main() {
+    Student S;
+    S.loc();
+    return 0;
+}
