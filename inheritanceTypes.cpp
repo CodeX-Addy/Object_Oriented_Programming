a211786b8606a0cfa468d9544c352e271173c6b4
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-30 19:29:34 +0530

    Update inheritanceTypes.cpp

--- inheritanceTypes.cpp
+++ inheritanceTypes.cpp
@@ -74,3 +74,40 @@ int main(){
     h1.intclass();
     h1.floatclass();
  }
+
+//Hierarchical Inheritance
+#include<iostream>
+using namespace std;
+
+class A{
+    public:
+    void func1(){
+        cout << "Function 1" << endl;
+    }
+};
+
+class B : public A{
+    public:
+    void func2(){
+        cout << "Function 2" << endl;
+    }
+};
+
+class C : public A{
+    public:
+    void func3(){
+        cout << "Function 3" << endl;
+    }
+};
+
+int main(){
+    A o1;
+    o1.func1();
+    B o2;
+    o2.func1();
+    o2.func2();
+    C o3;
+    o3.func1();
+    o3.func3();
+}
+
