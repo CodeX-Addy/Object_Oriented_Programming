dfd7fa8ef63fb62b37bcd0e05926745205418bf9
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-05-31 18:20:32 +0530

    Update compiletimePolymorphism.cpp

--- compiletimePolymorphism.cpp
+++ compiletimePolymorphism.cpp
@@ -18,3 +18,25 @@ int main(){
     cout << sum.add(10,20) << endl;
     cout << sum.add(10,20,30) << endl;
 }
+
+//Operator overload
+#include<iostream>
+using namespace std;
+
+class A {
+    public:
+    int a;
+    public:
+    void operator+ (A &obj){
+        int val1 = this->a;
+        int val2 = obj.a;
+        cout << val2 - val1;
+    }
+};
+
+int main(){
+    A obj1, obj2;
+    obj1.a = 8;
+    obj2.a = 10;
+    obj1 + obj2;
+}
