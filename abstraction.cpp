53d9a4f3f68721cc101562002faf6e5abafedc93
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2023-06-11 16:51:49 +0530

    Create abstraction.cpp

---
+++ abstraction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class abstraction{
+  private:
+  int a,b;
+  public:
+  void set(int x, int y){
+    a = x;
+    b = y;
+  }
+  void display(){
+    cout << a << endl;
+    cout << b << endl;
+  }
+};
+
+int main(){
+  abstraction obj;
+  obj.set(10,20);
+  obj.display();
+  return 0;
+}
