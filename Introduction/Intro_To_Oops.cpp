69f5c79e4d7ed8a8708ee1a850fbe34fb6e0f6d6
Aditya Tomar <98268205+CodeX-Addy@users.noreply.github.com>
2025-02-20 22:45:18 +0530

    add: intro to oops

--- Introduction/Intro_To_Oops.cpp
+++ Introduction/Intro_To_Oops.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
 using namespace std;
+
 // Using class we can create an user defined variable
 class Intro{ 
     public:// Access modifier
-    int num;
+        int num;
 };
 
 int main() {
     Intro n;// Declaring object
     n.num = 100; // Instantiation of class
     cout << n.num << endl;
-
     return 0;
 }
 
-
+// Another File
 #include <iostream>
 using namespace std;
 
 class Intro{
     public:
-    int num;
+        int num;
 };
 
 int main() {
     Intro n;
     n.num = 100;
     cout << n.num << endl;
-
     return 0;
 }
 
@@ -35,12 +34,12 @@ int main() {
 #include <iostream>
 using namespace std;
 
-class name{
+class Name{
     
 };
 
 int main() {
-    cout << sizeof(name) << endl;
+    cout << sizeof(Name) << endl;
     return 0;
 }
 //Output:- 1
